1d3c9e292aca05fcd61fc425e69db6e924a04af5
Christian Ehrig <ehrigc@mediamarktsaturn.com>
2019-11-19 22:20:07 +0100

    using different request header to avoid confusion

--- workers/another-worker.js
+++ workers/another-worker.js
@@ -2,13 +2,13 @@ addEventListener('fetch', event => {
     event.respondWith(processWorker(event.request))
 });
 
-// We are re-using the AB-Test Module for this worker
+// For this worker we are using the header override module
 const _mod_hdr_override = require("mod-hdr-override");
 
 async function processWorker(request) {
     let response;
 
-    request = _mod_hdr_override.setHeader(request, 'Host', 'example.com');
+    request = _mod_hdr_override.setHeader(request, 'X-REQUEST-HEADER', 'test');
 
     response = await fetch(request);
     return response;
